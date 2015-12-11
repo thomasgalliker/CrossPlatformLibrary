4f3e3ff48123db96f16d7e0ac741f99742d4123d
thomasgalliker <thomas@bluewin.ch>
2015-12-11 22:23:02 +0000

    Extracted HighlightMarker into new nuget package

--- CrossPlatformLibrary/Extensions/EnumerableExtensions.cs
+++ CrossPlatformLibrary/Extensions/EnumerableExtensions.cs
@@ -1,10 +1,9 @@
-﻿using System;
+﻿using Guards;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-
-using Guards;
 
 namespace CrossPlatformLibrary.Extensions
 {
@@ -43,49 +42,6 @@ namespace CrossPlatformLibrary.Extensions
         public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> enumerable)
         {
             return new ObservableCollection<T>(enumerable);
-        }
-
-        /// <summary>
-        ///     Transforms an IEnumerable into groups of integer lists which are consecutivly ascending.
-        ///     Please make sure that it - depending on the application of this method - is neccessary to pre-sort the IEnumerable
-        ///     in order to retrieve the right groups.
-        /// </summary>
-        /// <param name="source">The source.</param>
-        /// <returns>The groups of consecutive integer lists.</returns>
-        public static IEnumerable<List<int>> ToConsecutiveGroups(this IEnumerable<int> source)
-        {
-            Guard.ArgumentNotNull(() => source);
-
-            using (var iterator = source.GetEnumerator())
-            {
-                if (!iterator.MoveNext())
-                {
-                    yield break;
-                }
-                else
-                {
-                    int current = iterator.Current;
-                    var group = new List<int> { current };
-
-                    while (iterator.MoveNext())
-                    {
-                        int next = iterator.Current;
-                        if (next < current || current + 1 < next)
-                        {
-                            yield return group;
-                            group = new List<int>();
-                        }
-
-                        current = next;
-                        group.Add(current);
-                    }
-
-                    if (group.Any())
-                    {
-                        yield return group;
-                    }
-                }
-            }
         }
 
         /// <summary>
--- Tests/CrossPlatformLibrary.Tests/Extensions/EnumerableExtensionsTests.cs
+++ Tests/CrossPlatformLibrary.Tests/Extensions/EnumerableExtensionsTests.cs
@@ -72,38 +72,5 @@ namespace CrossPlatformLibrary.Tests.Extensions
             Assert.True(resultCollection[5] == expectedCollection[5]);
             Assert.True(resultCollection[6] == expectedCollection[6]);
         }
-
-        [Fact]
-        public void ToConsecutiveGroupsExtensionTestWithEmptyList()
-        {
-            // Arrange.
-            var sourceCollection = new List<int>();
-
-            // Act.
-            var resultListOfGroups = sourceCollection.ToConsecutiveGroups().ToList();
-
-            // Assert.
-            resultListOfGroups.Should().NotBeNull();
-            resultListOfGroups.Should().HaveCount(0);
-        }
-
-        [Fact]
-        public void ToConsecutiveGroupsExtensionTest()
-        {
-            // Arrange.
-            var sourceCollection = new List<int> { 5, 6, 7, 10, -2, -1, 3, 0 };
-
-            // Act.
-            var resultListOfGroups = sourceCollection.ToConsecutiveGroups().ToList();
-
-            // Assert.
-            resultListOfGroups.Should().NotBeNull();
-            resultListOfGroups.Should().HaveCount(5);
-            Assert.True(resultListOfGroups[0].Count() == 3);
-            Assert.True(resultListOfGroups[1].Count() == 1);
-            Assert.True(resultListOfGroups[2].Count() == 2);
-            Assert.True(resultListOfGroups[3].Count() == 1);
-            Assert.True(resultListOfGroups[4].Count() == 1);
-        }
     }
 }
