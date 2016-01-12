46fa839fbc54f04f75a986d310cc370b24a9f9d1
thomasgalliker <thomas@bluewin.ch>
2016-01-12 15:04:36 +0100

    Add new sample project (Xamarin Forms)

---
+++ Samples/Sample/Sample/App.cs
@@ -0,0 +1,33 @@
+﻿using CrossPlatformLibrary.Bootstrapping;
+
+using Xamarin.Forms;
+
+namespace Sample
+{
+    public class App : Application
+    {
+        private readonly Bootstrapper bootstrapper;
+
+        public App()
+        {
+            this.bootstrapper = new Bootstrapper();
+
+            this.MainPage = new DemoPage();
+        }
+
+        protected override void OnStart()
+        {
+            this.bootstrapper.Startup();
+        }
+
+        protected override void OnSleep()
+        {
+            this.bootstrapper.Sleep();
+        }
+
+        protected override void OnResume()
+        {
+            this.bootstrapper.Resume();
+        }
+    }
+}
---
+++ Samples/Sample/Sample/DemoPage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using CrossPlatformLibrary.IoC;
+
+using Xamarin.Forms;
+
+namespace Sample
+{
+    class DemoPage : ContentPage
+    {
+        public DemoPage()
+        {
+            var titleLabel = new Label { Text = "CrossPlatformLibrary Demo", FontSize = 24};
+
+            var button = new Button
+            {
+                Text = "Throw InvalidOperationException"
+            };
+            button.Clicked += (sender, args) =>
+            {
+               throw new InvalidOperationException("Some exception text...");
+            };
+            
+            var stackPanel = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                Children = { titleLabel, button }
+            };
+
+            this.Content = stackPanel;
+        }
+    }
+}
