32edffa5184cc78afcaf534296fab7596e697b9f
thomasgalliker <thomas@bluewin.ch>
2016-04-15 10:39:21 +0200

    Add support for UWP.

---
+++ Samples/Sample.UWP/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Sample.UWP
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            throw new InvalidOperationException("Some exception text...");
+        }
+    }
+}
