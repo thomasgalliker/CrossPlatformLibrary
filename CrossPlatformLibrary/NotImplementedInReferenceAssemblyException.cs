f2f08e9298edbcecf2e0e911702af132bf38c8bd
thomasgalliker <thomas@bluewin.ch>
2016-08-10 07:57:31 +0200

    Add comment

--- CrossPlatformLibrary/NotImplementedInReferenceAssemblyException.cs
+++ CrossPlatformLibrary/NotImplementedInReferenceAssemblyException.cs
@@ -3,6 +3,10 @@ using System.Runtime.CompilerServices;
 
 namespace CrossPlatformLibrary
 {
+    /// <summary>
+    /// This exception is raise when a platform-agnostic function is called from a specific platform.
+    /// Try to reinstall the nuget package of this project and run the code again.
+    /// </summary>
     public class NotImplementedInReferenceAssemblyException : NotImplementedException
     {
         public NotImplementedInReferenceAssemblyException([CallerMemberName] string caller = null) : base(
