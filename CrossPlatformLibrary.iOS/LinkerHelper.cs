4b09362cbf972a1f4d944d884c53fd60a0b7c616
Thomas Galliker <thomas@bluewin.ch>
2018-07-03 07:06:15 +0200

    Remove old ios project

--- CrossPlatformLibrary.iOS/LinkerHelper.cs
+++
@@ -1,5 +0,0 @@
-﻿using MonoTouch.Foundation;
-
-// This assembly annotation is essential for CrossPlatformLibrary to work correctly in Xamarin.iOS and MonoTouch projects.
-// It prevents the Xamarin linker from removing 'unused' code.
-[assembly: Preserve(typeof(CrossPlatformLibrary.LinkerHook), AllMembers = true)]
