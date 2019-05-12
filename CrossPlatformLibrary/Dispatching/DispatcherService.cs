bb7d28b90b5a94a2d2132b9d87bd43e4d40462a1
Thomas Galliker <thomas@bluewin.ch>
2019-05-12 12:03:19 +0800

    Remove support for PCL

--- CrossPlatformLibrary/Dispatching/DispatcherService.cs
+++ CrossPlatformLibrary/Dispatching/DispatcherService.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace CrossPlatformLibrary.Dispatching
 {
+    /// <summary>
+    /// Provides an implementation of <see cref="IDispatcherService"/> for .Net Standard.
+    /// </summary>
     public class DispatcherService : IDispatcherService
     {
         public void CheckBeginInvokeOnUI(Action action)
         {
-            throw new NotImplementedInReferenceAssemblyException();
+            Task.Factory.StartNew(action,
+                CancellationToken.None, 
+                TaskCreationOptions.None,
+                TaskScheduler.FromCurrentSynchronizationContext());
         }
     }
 }
--- CrossPlatformLibrary/Localization/Localizer.cs
+++ CrossPlatformLibrary/Localization/Localizer.cs
@@ -1,20 +1,44 @@
 ﻿using System;
 using System.Globalization;
+using System.Threading;
 
 namespace CrossPlatformLibrary.Localization
 {
     public class Localizer : ILocalizer
     {
-        public CultureInfo GetCurrentCulture()
+        static readonly Lazy<ILocalizer> Implementation = new Lazy<ILocalizer>(CreateResourceLoader, LazyThreadSafetyMode.PublicationOnly);
+
+        public static ILocalizer Current
         {
-            throw new NotImplementedInReferenceAssemblyException();
+            get
+            {
+                return Implementation.Value;
+            }
+        }
+
+        static ILocalizer CreateResourceLoader()
+        {
+            return new Localizer();
         }
 
         public void SetCultureInfo(CultureInfo cultureInfo)
         {
-            throw new NotImplementedInReferenceAssemblyException();
+            Thread.CurrentThread.CurrentCulture = cultureInfo;
+            Thread.CurrentThread.CurrentUICulture = cultureInfo;
+
+            this.OnLocaleChanged(cultureInfo);
         }
 
         public event EventHandler<CultureInfoChangedEventArgs> CultureInfoChangedEvent;
+
+        protected virtual void OnLocaleChanged(CultureInfo ci)
+        {
+            this.CultureInfoChangedEvent?.Invoke(this, new CultureInfoChangedEventArgs(ci));
+        }
+
+        public CultureInfo GetCurrentCulture()
+        {
+            return Thread.CurrentThread.CurrentCulture;
+        }
     }
 }
