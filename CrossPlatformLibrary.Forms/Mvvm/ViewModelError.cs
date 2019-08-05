b0a92e520b5f085ed1ba21c6abd15851b918bc21
Thomas Galliker <thomas@bluewin.ch>
2019-08-05 17:41:54 +0200

    Add ctor to take Action instead of ICommand

--- CrossPlatformLibrary.Forms/Mvvm/ViewModelError.cs
+++ CrossPlatformLibrary.Forms/Mvvm/ViewModelError.cs
@@ -1,11 +1,18 @@
+using System;
 using System.Windows.Input;
 using CrossPlatformLibrary.Mvvm;
+using Xamarin.Forms;
 
 namespace CrossPlatformLibrary.Forms.Mvvm
 {
     public class ViewModelError : BindableBase
     {
-        public static readonly ViewModelError None = new ViewModelError(null, null);
+        public static readonly ViewModelError None = new ViewModelError(text: null, command: null);
+
+        public ViewModelError(string text, Action action)
+            : this(text, new Command(action))
+        {
+        }
 
         public ViewModelError(string text, ICommand command)
         {
