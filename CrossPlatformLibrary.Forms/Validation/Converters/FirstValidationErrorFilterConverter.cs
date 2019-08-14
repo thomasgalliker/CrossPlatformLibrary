15f68d5aade05685ab171d9b7832df6baadccc78
Thomas Galliker <thomas@bluewin.ch>
2019-08-14 14:59:56 +0200

    Add validation error UI converters

---
+++ CrossPlatformLibrary.Forms/Validation/Converters/FirstValidationErrorFilterConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace CrossPlatformLibrary.Forms.Validation.Converters
+{
+    /// <summary>
+    /// Reads validation errors and returns the string message of the first validation error.
+    /// </summary>
+    public class FirstValidationErrorFilterConverter : ValidationErrorsFilterConverter
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (base.Convert(value, targetType, parameter, culture) is IEnumerable<string> propertyErrors)
+            {
+                var firstPropertyError = propertyErrors.FirstOrDefault();
+                return firstPropertyError;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ CrossPlatformLibrary.Forms/Validation/Converters/ValidationErrorsFilterConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace CrossPlatformLibrary.Forms.Validation.Converters
+{
+    /// <summary>
+    /// Reads validation errors and returns the string messages of all errors.
+    /// </summary>
+    public class ValidationErrorsFilterConverter : IValueConverter
+    {
+        public virtual object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is ViewModelValidation validation)
+            {
+                var parameterString = parameter as string;
+                if (!string.IsNullOrEmpty(parameterString))
+                {
+                    var properties = parameterString.Split('|');
+
+                    var propertyErrors = properties.SelectMany(p => validation.Errors[p]).ToList();
+                    return propertyErrors;
+                }
+
+                throw new InvalidOperationException("ConverterParameter must a pipe-delimited string");
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("ConvertBack is not supported");
+        }
+    }
+}
