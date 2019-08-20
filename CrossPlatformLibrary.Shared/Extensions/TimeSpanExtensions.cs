40aa0ec23276d6edd0a6b56ae6f819534a5fe2ed
Thomas Galliker <thomas@bluewin.ch>
2019-08-20 09:52:55 +0200

    Add ToDurationString extension methods for TimeSpan

--- CrossPlatformLibrary.Shared/Extensions/TimeSpanExtensions.cs
+++ CrossPlatformLibrary.Shared/Extensions/TimeSpanExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 
 namespace CrossPlatformLibrary.Extensions
 {
@@ -35,6 +36,116 @@ namespace CrossPlatformLibrary.Extensions
         public static string ToSecondsString(this TimeSpan? timeSpan)
         {
             return timeSpan == null ? "" : timeSpan.Value.ToSecondsString();
+        }
+
+        public static string ToDurationString(this TimeSpan? timeSpan)
+        {
+            return timeSpan?.ToDurationString();
+        }
+
+        public static string ToDurationString(this TimeSpan timeSpan)
+        {
+            if (timeSpan == TimeSpan.MinValue)
+            {
+                return "";
+            }
+
+            if (timeSpan == TimeSpan.Zero)
+            {
+                return "0s";
+            }
+
+            var absoluteTimeSpan = timeSpan.Duration();
+            var daysString = AddDaysIfNotZero(absoluteTimeSpan);
+            if (daysString != null)
+            {
+                return daysString;
+            }
+
+            var hoursString = GetHours(absoluteTimeSpan);
+            if (hoursString != null)
+            {
+                return hoursString;
+            }
+
+            var minutesString = GetMinutes(absoluteTimeSpan);
+            if (minutesString != null)
+            {
+                return minutesString;
+            }
+
+            var secondsString = GetSeconds(absoluteTimeSpan);
+            if (secondsString != null)
+            {
+                return secondsString;
+            }
+
+            var microsecondsString = GetMicroseconds(absoluteTimeSpan);
+            if (microsecondsString != null)
+            {
+                return microsecondsString;
+            }
+
+            return "";
+        }
+
+        private static string GetMicroseconds(TimeSpan timeSpan)
+        {
+            var num1 = timeSpan.Ticks % 10000L;
+            if (num1 <= 0L)
+            {
+                return null;
+            }
+
+            var num2 = num1 / 10000.0 * 1000.0;
+            return num2.ToString("0.0", CultureInfo.InvariantCulture) + "µs";
+        }
+
+        private static string GetSeconds(TimeSpan timeSpan)
+        {
+            if (timeSpan.Seconds <= 0 && timeSpan.Milliseconds <= 0)
+            {
+                return null;
+            }
+
+            var seconds = timeSpan.Seconds;
+            var secondsString = seconds.ToString(CultureInfo.InvariantCulture);
+            if (timeSpan.Milliseconds > 0)
+            {
+                secondsString = $"{secondsString}.{timeSpan.Milliseconds.ToString("000", CultureInfo.InvariantCulture)}";
+            }
+
+            return secondsString + "s";
+        }
+
+        private static string GetMinutes(TimeSpan timeSpan)
+        {
+            if (timeSpan.Minutes <= 0)
+            {
+                return null;
+            }
+
+            return timeSpan.Minutes.ToString(CultureInfo.InvariantCulture) + "m";
+        }
+
+        private static string GetHours(TimeSpan timeSpan)
+        {
+            if (timeSpan.Hours <= 0)
+            {
+                return null;
+            }
+
+            return timeSpan.Hours.ToString(CultureInfo.InvariantCulture) + "h";
+        }
+
+        private static string AddDaysIfNotZero(TimeSpan timeSpan)
+        {
+            if (timeSpan.Days <= 0)
+            {
+                return null;
+            }
+
+            return timeSpan.Days.ToString(CultureInfo.InvariantCulture) + "d";
         }
     }
 }
--- Tests/CrossPlatformLibrary.Tests/Extensions/TimeSpanExtensionsTests.cs
+++ Tests/CrossPlatformLibrary.Tests/Extensions/TimeSpanExtensionsTests.cs
@@ -32,5 +32,39 @@ namespace CrossPlatformLibrary.Tests.Extensions
                 this.Add(TimeSpan.MaxValue, "922337203685.5s");
             }
         }
+
+        [Theory]
+        [ClassData(typeof(ToDurationStringTestData))]
+        public void ShouldConvertTimeSpanToDurationString(TimeSpan timeSpan, string expectedDurationString)
+        {
+            // Act
+            var durationString = timeSpan.ToDurationString();
+
+            // Assert
+            durationString.Should().Be(expectedDurationString);
+        }
+
+        public class ToDurationStringTestData : TheoryData<TimeSpan, string>
+        {
+            public ToDurationStringTestData()
+            {
+                // Special cases
+                this.Add(TimeSpan.MinValue, "");
+                this.Add(TimeSpan.MaxValue, "10675199d");
+                this.Add(TimeSpan.Zero, "0s");
+
+                // Simple TimeSpans
+                this.Add(TimeSpan.FromDays(1), "1d");
+                this.Add(TimeSpan.FromHours(1), "1h");
+                this.Add(TimeSpan.FromMinutes(1), "1m");
+                this.Add(TimeSpan.FromSeconds(1), "1s");
+                this.Add(TimeSpan.FromMilliseconds(1), "0.001s");
+                this.Add(TimeSpan.FromTicks(1L), "0.1µs");
+
+                // Complex TimeSpans
+                this.Add(new TimeSpan(1, 2, 3, 4), "1d");
+                this.Add(new TimeSpan(788, 97, 66, 100, 1234), "792d");
+            }
+        }
     }
 }
