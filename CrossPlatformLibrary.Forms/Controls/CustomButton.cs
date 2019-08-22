0e36508c8f829af4ac96ff28637bf025799c67ae
Thomas Galliker <thomas@bluewin.ch>
2019-08-22 17:01:10 +0200

    Cleanup

--- CrossPlatformLibrary.Forms/Controls/CustomButton.cs
+++ CrossPlatformLibrary.Forms/Controls/CustomButton.cs
@@ -4,20 +4,30 @@ namespace CrossPlatformLibrary.Forms.Controls
 {
     public class CustomButton : Button
     {
-        public static readonly BindableProperty BackgroundColorPressedProperty = BindableProperty.Create(nameof(BackgroundColorPressed), typeof(Color), typeof(CustomButton), Color.White);
+        public static readonly BindableProperty BackgroundColorPressedProperty =
+            BindableProperty.Create(
+                nameof(BackgroundColorPressed),
+                typeof(Color),
+                typeof(CustomButton),
+                Color.White);
 
         public Color BackgroundColorPressed
         {
-            get { return (Color)this.GetValue(BackgroundColorPressedProperty); }
-            set { this.SetValue(BackgroundColorPressedProperty, value); }
+            get => (Color)this.GetValue(BackgroundColorPressedProperty);
+            set => this.SetValue(BackgroundColorPressedProperty, value);
         }
 
-        public static readonly BindableProperty BorderColorPressedProperty = BindableProperty.Create(nameof(BorderColorPressed), typeof(Color), typeof(CustomButton), Color.White);
+        public static readonly BindableProperty BorderColorPressedProperty =
+            BindableProperty.Create(
+                nameof(BorderColorPressed),
+                typeof(Color),
+                typeof(CustomButton),
+                Color.White);
 
         public Color BorderColorPressed
         {
-            get { return (Color)this.GetValue(BorderColorPressedProperty); }
-            set { this.SetValue(BorderColorPressedProperty, value); }
+            get => (Color)this.GetValue(BorderColorPressedProperty);
+            set => this.SetValue(BorderColorPressedProperty, value);
         }
 
         public static readonly BindableProperty VerticalContentAlignmentProperty = BindableProperty.Create(
@@ -28,8 +38,8 @@ namespace CrossPlatformLibrary.Forms.Controls
 
         public TextAlignment VerticalContentAlignment
         {
-            get { return (TextAlignment)this.GetValue(VerticalContentAlignmentProperty); }
-            set { this.SetValue(VerticalContentAlignmentProperty, value); }
+            get => (TextAlignment)this.GetValue(VerticalContentAlignmentProperty);
+            set => this.SetValue(VerticalContentAlignmentProperty, value);
         }
 
         public static readonly BindableProperty HorizontalContentAlignmentProperty = BindableProperty.Create(
@@ -40,10 +50,9 @@ namespace CrossPlatformLibrary.Forms.Controls
 
         public TextAlignment HorizontalContentAlignment
         {
-            get { return (TextAlignment)this.GetValue(HorizontalContentAlignmentProperty); }
-            set { this.SetValue(HorizontalContentAlignmentProperty, value); }
+            get => (TextAlignment)this.GetValue(HorizontalContentAlignmentProperty);
+            set => this.SetValue(HorizontalContentAlignmentProperty, value);
         }
-
 
         public static readonly BindableProperty AllCapsProperty =
             BindableProperty.Create(
@@ -64,9 +73,8 @@ namespace CrossPlatformLibrary.Forms.Controls
 
         public bool AllCaps
         {
-            get { return (bool)this.GetValue(AllCapsProperty); }
-            set { this.SetValue(AllCapsProperty, value); }
+            get => (bool)this.GetValue(AllCapsProperty);
+            set => this.SetValue(AllCapsProperty, value);
         }
     }
 }
-
