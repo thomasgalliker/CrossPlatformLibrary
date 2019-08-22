b74c1cdb957c72d3bccd45a1c3efa9ba61719461
Thomas Galliker <thomas@bluewin.ch>
2019-08-22 12:03:54 +0200

    Add GuidExtensions.Increment to increment guids

---
+++ CrossPlatformLibrary.Shared/Extensions/GuidExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace InvoiceScanner.Extensions
+{
+    public static class GuidExtensions
+    {
+        private static readonly int[] GuidByteOrder = { 15, 14, 13, 12, 11, 10, 9, 8, 6, 7, 4, 5, 0, 1, 2, 3 };
+
+        /// <summary>
+        ///     Increments the given <paramref name="guid" /> to by one.
+        ///     Original source: http://stackoverflow.com/questions/30404965/increment-guid-in-c-sharp#30405028
+        /// </summary>
+        /// <example>
+        ///     Guid.Increment can be helpful for generating human-readable Guids like "00000000-0000-0000-0000-000000000001"
+        ///     which can be incremented to "00000000-0000-0000-0000-000000000002".
+        /// </example>
+        /// <param name="guid">The input Guid.</param>
+        /// <returns>The incremented output Guid.</returns>
+        public static Guid Increment(this Guid guid)
+        {
+            var bytes = guid.ToByteArray();
+            var carry = true;
+            for (var i = 0; i < GuidByteOrder.Length && carry; i++)
+            {
+                var index = GuidByteOrder[i];
+                var oldValue = bytes[index]++;
+                carry = oldValue > bytes[index];
+            }
+
+            return new Guid(bytes);
+        }
+    }
+}
---
+++ Tests/CrossPlatformLibrary.Tests/Extensions/GuidExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using FluentAssertions;
+using InvoiceScanner.Extensions;
+using Xunit;
+
+namespace CrossPlatformLibrary.Tests.Extensions
+{
+    public class GuidExtensionsTests
+    {
+        [Theory]
+        [ClassData(typeof(IncrementTestData))]
+        public void ShouldIncrementGuid(Guid input, Guid expectedOutput)
+        {
+            // Act
+            var output = input.Increment();
+
+            // Assert
+            output.Should().Be(expectedOutput);
+        }
+
+        public class IncrementTestData : TheoryData<Guid, Guid>
+        {
+            public IncrementTestData()
+            {
+                this.Add(Guid.Empty, new Guid("00000000-0000-0000-0000-000000000001"));
+                this.Add(new Guid("00000000-0000-0000-0000-000000000001"), new Guid("00000000-0000-0000-0000-000000000002"));
+                this.Add(new Guid("00000000-0000-0000-0000-00000000000A"), new Guid("00000000-0000-0000-0000-00000000000B"));
+                this.Add(new Guid("AAF9E39A-BD82-4A44-8E8F-516EC3795390"), new Guid("aaf9e39a-bd82-4a44-8e8f-516ec3795391"));
+            }
+        }
+    }
+}
