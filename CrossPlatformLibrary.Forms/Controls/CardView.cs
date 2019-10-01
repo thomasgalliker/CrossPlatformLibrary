507d2325dadcff60bce9ce3deabadeafda1d8764
Thomas Galliker <thomas@bluewin.ch>
2019-10-01 12:54:44 +0200

    CardView with IsClippedToBounds=true

--- CrossPlatformLibrary.Forms/Controls/CardView.cs
+++ CrossPlatformLibrary.Forms/Controls/CardView.cs
@@ -8,26 +8,24 @@ namespace CrossPlatformLibrary.Forms.Controls
         public CardView()
         {
             this.Padding = 0;
+            this.BorderColor = Color.Transparent;
+            this.IsClippedToBounds = true;
 
             if (Device.RuntimePlatform == Device.iOS)
             {
                 this.HasShadow = false;
                 this.CornerRadius = 0;
-                this.BorderColor = Color.Transparent;
                 this.BackgroundColor = Color.Transparent;
             }
             else
             {
                 this.HasShadow = true;
                 this.OutlineColor = Color.White; // BUG: If this is not set, HasShadow has no effect
-                this.BorderColor = Color.Transparent;
                 this.CornerRadius = 6;
 
                 // Hack: OnPlatform lacks of support for DynamicResource bindings!
-                if (Device.RuntimePlatform == Device.Android)
-                {
-                    this.SetDynamicResource(Frame.BackgroundColorProperty, ThemeConstants.CardViewStyle.BackgroundColor);
-                }
+                this.SetDynamicResource(Frame.BackgroundColorProperty, ThemeConstants.CardViewStyle.BackgroundColor);
+                this.SetDynamicResource(Frame.OutlineColorProperty, ThemeConstants.CardViewStyle.BackgroundColor);
             }
         }
     }
