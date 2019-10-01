c1f2840e9c36c7a6b10ff1aa2bce69e18d8114ef
Thomas Galliker <thomas@bluewin.ch>
2019-10-01 18:03:02 +0200

    Add iOS.PageRenderer to support back navigation gestures

---
+++ CrossPlatformLibrary.Forms.iOS/Renderers/PageRenderer.cs
@@ -0,0 +1,22 @@
+﻿using CrossPlatformLibrary.Forms.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+
+[assembly: ExportRenderer(typeof(ContentPage), typeof(PageRenderer))]
+
+namespace CrossPlatformLibrary.Forms.iOS.Renderers
+{
+    public class PageRenderer : Xamarin.Forms.Platform.iOS.PageRenderer
+    {
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+
+            if (this.ViewController?.NavigationController?.InteractivePopGestureRecognizer is UIGestureRecognizer uiGestureRecognizer)
+            {
+                uiGestureRecognizer.Enabled = true;
+                uiGestureRecognizer.Delegate = new UIGestureRecognizerDelegate();
+            }
+        }
+    }
+}
