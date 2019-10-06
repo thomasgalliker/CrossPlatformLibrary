b92ae16b24bade00951ecf165e5601102f70d1fc
Thomas Galliker <thomas@bluewin.ch>
2019-10-06 17:19:26 +0200

    Add ButtonRenderer to workaround the issue related to missing button cancel events

--- CrossPlatformLibrary.Forms.iOS/Renderers/CustomButtonRenderer.cs
+++ CrossPlatformLibrary.Forms.iOS/Renderers/CustomButtonRenderer.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using CrossPlatformLibrary.Forms.Controls;
 using CrossPlatformLibrary.Forms.iOS.Extensions;
 using CrossPlatformLibrary.Forms.iOS.Renderers;
@@ -6,10 +7,53 @@ using UIKit;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
 
+[assembly: ExportRenderer(typeof(Button), typeof(CrossPlatformLibrary.Forms.iOS.Renderers.ButtonRenderer))]
 [assembly: ExportRenderer(typeof(CustomButton), typeof(CustomButtonRenderer))]
 
 namespace CrossPlatformLibrary.Forms.iOS.Renderers
 {
+    /// <summary>
+    /// ButtonRenderer: Implements a workaround for missing Released event if button was released outside the visible area (MotionEventActions.Cancel).
+    /// See here: https://github.com/xamarin/Xamarin.Forms/issues/3523
+    /// </summary>
+    public class ButtonRenderer : Xamarin.Forms.Platform.iOS.ButtonRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+
+            if (this.Control != null && e.NewElement is Button button)
+            {
+                this.Control.TouchCancel += this.SendReleased;
+                this.Control.TouchDragExit += this.SendReleased;
+                this.Control.TouchDragEnter += this.SendPressed;
+            }
+        }
+
+        private void SendReleased(object sender, EventArgs e)
+        {
+            ((IButtonController)this.Element)?.SendReleased();
+        }
+
+        private void SendPressed(object sender, EventArgs e)
+        {
+            ((IButtonController)this.Element)?.SendPressed();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (this.Control != null)
+            {
+                this.Control.TouchDragExit -= this.SendReleased;
+                this.Control.TouchCancel -= this.SendReleased;
+                this.Control.TouchDragEnter -= this.SendPressed;
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+
+
     /// <summary>
     ///     Source: http://www.wintellect.com/devcenter/jprosise/supercharging-xamarin-forms-with-custom-renderers-part-1
     /// </summary>
@@ -36,14 +80,16 @@ namespace CrossPlatformLibrary.Forms.iOS.Renderers
         {
             base.OnElementPropertyChanged(sender, e);
 
-            var customButton = this.Element as CustomButton;
-            if (customButton != null)
+            if (e.PropertyName == CustomButton.VerticalContentAlignmentProperty.PropertyName)
             {
-                if (e.PropertyName == CustomButton.VerticalContentAlignmentProperty.PropertyName)
+                if (this.Element is CustomButton customButton)
                 {
                     this.UpdateVerticalAlignment(customButton);
                 }
-                else if (e.PropertyName == CustomButton.HorizontalContentAlignmentProperty.PropertyName)
+            }
+            else if (e.PropertyName == CustomButton.HorizontalContentAlignmentProperty.PropertyName)
+            {
+                if (this.Element is CustomButton customButton)
                 {
                     this.UpdateHorizontalAlignment(customButton);
                 }
