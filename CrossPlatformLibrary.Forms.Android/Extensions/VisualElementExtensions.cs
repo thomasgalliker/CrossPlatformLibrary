ba375059cb9cb304da786eb9435a18a657ac5773
Thomas Galliker <thomas@bluewin.ch>
2019-10-17 09:47:49 +0200

    Add VisualElementExtensions for Android and iOS

---
+++ CrossPlatformLibrary.Forms.Android/Extensions/VisualElementExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Android.Content;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+namespace CrossPlatformLibrary.Forms.Android.Extensions
+{
+    public static class VisualElementExtensions
+    {
+        public static IVisualElementRenderer GetOrCreateRenderer(this VisualElement bindable, Context context)
+        {
+            var renderer = Platform.GetRenderer(bindable);
+            if (renderer == null)
+            {
+                renderer = Platform.CreateRendererWithContext(bindable, context);
+                Platform.SetRenderer(bindable, renderer);
+            }
+
+            return renderer;
+        }
+    }
+}
---
+++ CrossPlatformLibrary.Forms.iOS/Extensions/VisualElementExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+namespace CrossPlatformLibrary.Forms.iOS.Extensions
+{
+    public static class VisualElementExtensions
+    {
+        public static IVisualElementRenderer GetOrCreateRenderer(this VisualElement bindable)
+        {
+            var renderer = Platform.GetRenderer(bindable);
+            if (renderer == null)
+            {
+                renderer = Platform.CreateRenderer(bindable);
+                Platform.SetRenderer(bindable, renderer);
+            }
+
+            return renderer;
+        }
+    }
+}
