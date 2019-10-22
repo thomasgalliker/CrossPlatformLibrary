e42172363fb290ca199c0560aecba4044b07a804
Thomas Galliker <thomas@bluewin.ch>
2019-10-22 16:29:02 +0200

    Add new control: ErrorBox

---
+++ CrossPlatformLibrary.Forms/Controls/ErrorBox.xaml.cs
@@ -0,0 +1,29 @@
+﻿
+using CrossPlatformLibrary.Forms.Validation;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CrossPlatformLibrary.Forms.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ErrorBox : Frame
+    {
+        public ErrorBox()
+        {
+            this.InitializeComponent();
+        }
+
+        public static readonly BindableProperty TextProperty =
+            BindableProperty.Create(
+                nameof(Text),
+                typeof(string),
+                typeof(ErrorBox),
+                default(string));
+
+        public string Text
+        {
+            get => (string)this.GetValue(TextProperty);
+            set => this.SetValue(TextProperty, value);
+        }
+    }
+}
