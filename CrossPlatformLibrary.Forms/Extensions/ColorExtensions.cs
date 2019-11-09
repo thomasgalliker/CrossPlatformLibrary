7f53a1139925895d38bd8b2267f1364531f1cf5b
Thomas Galliker <thomas@bluewin.ch>
2019-11-09 14:37:04 +0100

    Add extension methods for Color: Invert and Complement

--- CrossPlatformLibrary.Forms/Extensions/ColorExtensions.cs
+++ CrossPlatformLibrary.Forms/Extensions/ColorExtensions.cs
@@ -4,21 +4,28 @@ namespace CrossPlatformLibrary.Forms.Extensions
 {
     public static class ColorExtensions
     {
-        public static string ToHexString(this Color color, bool outputAlpha = true)
+        public static Color Complement(this Color color)
         {
-            string hex = "#";
-            if (outputAlpha)
-            {
-                hex += DoubleToHex(color.A);
-            }
+            var hue = (color.Hue * 359.0);
+            var newHue = ((hue + 180) % 359.0);
+            var complement = color.WithHue(newHue / 359.0);
 
-            var hexString = $"{hex}{DoubleToHex(color.R)}{DoubleToHex(color.G)}{DoubleToHex(color.B)}";
-            return hexString;
+            return complement;
         }
 
-        private static string DoubleToHex(double value)
+        /// <summary>
+        /// Inverts the R-G-B parts of <paramref name="color"/>.
+        /// The Alpha channel remains the same as in <paramref name="color"/>.
+        /// </summary>
+        /// <param name="color">The input color.</param>
+        /// <returns>An inverted color.</returns>
+        public static Color Invert(this Color color)
         {
-            return string.Format("{0:X2}", (int)(value * 255));
+            var r = 255 - (int)(255 * color.R);
+            var g = 255 - (int)(255 * color.G);
+            var b = 255 - (int)(255 * color.B);
+
+            return Color.FromRgba(r, g, b, color.A);
         }
     }
 }
---
+++ Tests/CrossPlatformLibrary.Forms.Tests/Extensions/ColorExtensionsTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+using CrossPlatformLibrary.Forms.Extensions;
+using FluentAssertions;
+using Xamarin.Forms;
+using Xunit;
+
+namespace CrossPlatformLibrary.Forms.Tests.Extensions
+{
+    public class ColorExtensionsTests
+    {
+        [Fact]
+        public void ShouldComplement()
+        {
+            // Arrange
+            var color = Color.FromHex("#FF0000");
+
+            // Act
+            var invertedColor = color.Complement();
+
+            // Assert
+            invertedColor.A.Should().Be(1);
+            invertedColor.R.Should().Be(0);
+            invertedColor.G.Should().Be(0.99164342880249023);
+            invertedColor.B.Should().Be(1);
+            invertedColor.Saturation.Should().Be(1.0);
+            invertedColor.Luminosity.Should().Be(0.5);
+        }
+
+        [Fact]
+        public void ShouldInvert()
+        {
+            // Arrange
+            var color = Color.FromHex("#FF0000");
+
+            // Act
+            var invertedColor = color.Invert();
+
+            // Assert
+            invertedColor.Should().Be(Color.FromHex("#00FFFF"));
+        }
+
+        [Fact]
+        public void ShouldInvert_WithAlpha()
+        {
+            // Arrange
+            var color = Color.FromHex("#55FF0000");
+
+            // Act
+            var invertedColor = color.Invert();
+
+            // Assert
+            invertedColor.Should().Be(Color.FromHex("#5500FFFF"));
+        }
+    }
+}
