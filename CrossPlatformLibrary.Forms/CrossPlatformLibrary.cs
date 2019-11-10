3a2fa59995569b978012a1d02408e897311de517
Thomas Galliker <thomas@bluewin.ch>
2019-11-10 20:19:04 +0100

    Add performance logging

--- CrossPlatformLibrary.Forms/CrossPlatformLibrary.cs
+++ CrossPlatformLibrary.Forms/CrossPlatformLibrary.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using CrossPlatformLibrary.Forms.Resources;
 using CrossPlatformLibrary.Forms.Themes;
 using CrossPlatformLibrary.Forms.Themes.Extensions;
@@ -59,8 +60,10 @@ namespace CrossPlatformLibrary.Forms
                 throw new ArgumentNullException(nameof(crossPlatformLibraryResource));
             }
 
+            var stopwatch = Stopwatch.StartNew();
             var crossPlatformLibrary = new CrossPlatformLibrary(app, crossPlatformLibraryResource);
             crossPlatformLibrary.MergeCrossPlatformLibraryDictionaries();
+            Debug.WriteLine($"Init finished in {stopwatch.Elapsed.TotalMilliseconds:F0}ms");
         }
 
         /// <summary>
@@ -85,8 +88,10 @@ namespace CrossPlatformLibrary.Forms
                 throw new ArgumentNullException(nameof(key));
             }
 
+            var stopwatch = Stopwatch.StartNew();
             var crossPlatformLibrary = new CrossPlatformLibrary(app, key);
             crossPlatformLibrary.MergeCrossPlatformLibraryDictionaries();
+            Debug.WriteLine($"Init finished in {stopwatch.Elapsed.TotalMilliseconds:F0}ms");
         }
 
         /// <summary>
@@ -101,8 +106,10 @@ namespace CrossPlatformLibrary.Forms
                 throw new ArgumentNullException(nameof(app));
             }
 
+            var stopwatch = Stopwatch.StartNew();
             var crossPlatformLibrary = new CrossPlatformLibrary(app);
             crossPlatformLibrary.MergeCrossPlatformLibraryDictionaries();
+            Debug.WriteLine($"Init finished in {stopwatch.Elapsed.TotalMilliseconds:F0}ms");
         }
 
         private void MergeCrossPlatformLibraryDictionaries()
@@ -111,77 +118,5 @@ namespace CrossPlatformLibrary.Forms
             this.applicationResources.MergedDictionaries.Add(new ThemeSpacingResources(this.config.SpacingConfiguration ?? new SpacingConfiguration()));
             this.applicationResources.MergedDictionaries.Add(new ThemeFontResources(this.config.FontConfiguration ?? new FontConfiguration()));
         }
-
-
-        /// <summary>
-        ///     Static class that contains the current CrossPlatformLibrary font family values.
-        /// </summary>
-        //public static class FontFamily
-        //{
-        //    /// <summary>
-        //    ///     Body 1 font family, used for long-form writing and small text sizes.
-        //    /// </summary>
-        //    public static string Body1 => GetResource<string>(ThemeConstants.FontFamily.BODY1);
-
-        //    /// <summary>
-        //    ///     Body 2 font family, used for long-form writing and small text sizes.
-        //    /// </summary>
-        //    public static string Body2 => GetResource<string>(ThemeConstants.FontFamily.BODY2);
-
-        //    /// <summary>
-        //    ///     Button font family, used by different types of buttons.
-        //    /// </summary>
-        //    public static string Button => GetResource<string>(ThemeConstants.FontFamily.BUTTON);
-
-        //    /// <summary>
-        //    ///     Caption font family, used for annotations or to introduce a headline text.
-        //    /// </summary>
-        //    public static string Caption => GetResource<string>(ThemeConstants.FontFamily.CAPTION);
-
-        //    /// <summary>
-        //    ///     Headline 1 font family, used by large text on the screen.
-        //    /// </summary>
-        //    public static string H1 => GetResource<string>(ThemeConstants.FontFamily.H1);
-
-        //    /// <summary>
-        //    ///     Headline 2 font family, used by large text on the screen.
-        //    /// </summary>
-        //    public static string H2 => GetResource<string>(ThemeConstants.FontFamily.H2);
-
-        //    /// <summary>
-        //    ///     Headline 3 font family, used by large text on the screen.
-        //    /// </summary>
-        //    public static string H3 => GetResource<string>(ThemeConstants.FontFamily.H3);
-
-        //    /// <summary>
-        //    ///     Headline 4 font family, used by large text on the screen.
-        //    /// </summary>
-        //    public static string H4 => GetResource<string>(ThemeConstants.FontFamily.H4);
-
-        //    /// <summary>
-        //    ///     Headline 5 font family, used by large text on the screen.
-        //    /// </summary>
-        //    public static string H5 => GetResource<string>(ThemeConstants.FontFamily.H5);
-
-        //    /// <summary>
-        //    ///     Headline 6 font family, used by large text on the screen.
-        //    /// </summary>
-        //    public static string H6 => GetResource<string>(ThemeConstants.FontFamily.H6);
-
-        //    /// <summary>
-        //    ///     Overline font family, used for annotations or to introduce a headline text.
-        //    /// </summary>
-        //    public static string Overline => GetResource<string>(ThemeConstants.FontFamily.OVERLINE);
-
-        //    /// <summary>
-        //    ///     Subtitle 1 font family, used by medium-emphasis text.
-        //    /// </summary>
-        //    public static string Subtitle1 => GetResource<string>(ThemeConstants.FontFamily.SUBTITLE1);
-
-        //    /// <summary>
-        //    ///     Subtitle 2 font family, used by medium-emphasis text.
-        //    /// </summary>
-        //    public static string Subtitle2 => GetResource<string>(ThemeConstants.FontFamily.SUBTITLE2);
-        //}
     }
 }
