ac00469493b9d24e612151cb22ad6178c4a66e4a
Thomas Galliker <thomas@bluewin.ch>
2019-11-16 14:59:44 +0100

    Code cleanup in gradient stacklayout renderer

--- CrossPlatformLibrary.Forms.Android/Renderers/GradientStackLayoutRenderer.cs
+++ CrossPlatformLibrary.Forms.Android/Renderers/GradientStackLayoutRenderer.cs
@@ -13,29 +13,27 @@ namespace CrossPlatformLibrary.Forms.Android.Renderers
 {
     public class GradientStackLayoutRenderer : VisualElementRenderer<StackLayout>
     {
+        private StackOrientation gradientOrientation;
+        private Color startColor;
+        private Color endColor;
+
         public GradientStackLayoutRenderer(Context context) : base(context)
         {
         }
-
-        private StackOrientation GradientOrientation { get; set; }
-
-        private Color StartColor { get; set; }
-
-        private Color EndColor { get; set; }
 
         protected override void DispatchDraw(Canvas canvas)
         {
             // Linear distribution is configurable:
             // https://stackoverflow.com/questions/35974463/why-doesnt-this-argument-of-the-lineargradient-constructor-seem-to-work
 
-            var gradient = this.GradientOrientation == StackOrientation.Horizontal
+            var gradient = this.gradientOrientation == StackOrientation.Horizontal
                 ? new LinearGradient(x0: 0, y0: 0, x1: this.Width, y1: 0,
-                    color0: this.StartColor.ToAndroid(),
-                    color1: this.EndColor.ToAndroid(),
+                    color0: this.startColor.ToAndroid(),
+                    color1: this.endColor.ToAndroid(),
                     tile: Shader.TileMode.Clamp)
                 : new LinearGradient(x0: 0, y0: 0, x1: 0, y1: this.Height,
-                    color0: this.StartColor.ToAndroid(),
-                    color1: this.EndColor.ToAndroid(),
+                    color0: this.startColor.ToAndroid(),
+                    color1: this.endColor.ToAndroid(),
                     tile: Shader.TileMode.Clamp);
 
             var paint = new Paint
@@ -57,25 +55,11 @@ namespace CrossPlatformLibrary.Forms.Android.Renderers
                 return;
             }
 
-            try
+            if (e.NewElement is GradientStackLayout gradientColorStack)
             {
-                if (e.NewElement is GradientStackLayout gradientColorStack)
-                {
-                    this.StartColor = gradientColorStack.StartColor;
-                    this.EndColor = gradientColorStack.EndColor;
-                    this.GradientOrientation = gradientColorStack.GradientOrientation;
-
-                    //var gradient = new GradientDrawable(
-                    //    GradientDrawable.Orientation.TopBottom,
-                    //    new[] { this.StartColor.ToAndroid().ToArgb(), this.EndColor.ToAndroid().ToArgb() }
-                    //);
-
-                    //ViewCompat.SetBackground(this, gradient);
-                }
-            }
-            catch (Exception ex)
-            {
-                System.Diagnostics.Debug.WriteLine(@"ERROR:", ex.Message);
+                this.startColor = gradientColorStack.StartColor;
+                this.endColor = gradientColorStack.EndColor;
+                this.gradientOrientation = gradientColorStack.GradientOrientation;
             }
         }
     }
