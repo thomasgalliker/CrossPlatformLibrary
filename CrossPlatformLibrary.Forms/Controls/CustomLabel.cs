2c9f4df957686b65f59d70ce9b55263a1c83129f
Thomas Galliker <thomas@bluewin.ch>
2019-11-18 17:34:18 +0100

    Add RemovePadding to CustomLabel

--- CrossPlatformLibrary.Forms/Controls/CustomLabel.cs
+++ CrossPlatformLibrary.Forms/Controls/CustomLabel.cs
@@ -33,5 +33,20 @@ namespace CrossPlatformLibrary.Forms.Controls
             get => (bool)this.GetValue(JustifyTextProperty);
             set => this.SetValue(JustifyTextProperty, value);
         }
+
+        public static readonly BindableProperty RemovePaddingProperty =
+            BindableProperty.Create(
+                nameof(RemovePadding),
+                typeof(bool),
+                typeof(CustomLabel),
+                false,
+                BindingMode.OneWay
+            );
+
+        public bool RemovePadding
+        {
+            get => (bool)this.GetValue(RemovePaddingProperty);
+            set => this.SetValue(RemovePaddingProperty, value);
+        }
     }
 }
