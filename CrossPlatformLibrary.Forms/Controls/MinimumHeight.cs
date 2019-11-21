149fe587c2d2255cb8dab772ba259938b5d5a505
Thomas Galliker <thomas@bluewin.ch>
2019-11-21 15:27:03 +0100

    Reset height if property "Text" changed (MinimumHeight attached property)

--- CrossPlatformLibrary.Forms/Controls/MinimumHeight.cs
+++ CrossPlatformLibrary.Forms/Controls/MinimumHeight.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
 using CrossPlatformLibrary.Extensions;
 using Xamarin.Forms;
 
@@ -32,47 +34,58 @@ namespace CrossPlatformLibrary.Forms.Controls
 
         private static void OnMinimumHeightPropertyChanged(BindableObject bindable, object oldValue, object newValue)
         {
-            Log($"OnMinimumHeightPropertyChanged: bindable={bindable.GetType().GetFormattedName()}");
-
-            var oldMinimumHeight = oldValue as double?;
+            Log(bindable, $"OnMinimumHeightPropertyChanged: oldValue={oldValue}, newValue={newValue}");
 
             if (bindable is Layout layout)
             {
-                if (oldMinimumHeight != null)
+                if (oldValue != null)
                 {
                     layout.LayoutChanged -= MinimumHeight.OnLayoutChanged;
-                    layout.PropertyChanged -= MinimumHeight.OnPropertyChanged;
                 }
 
                 if (newValue != null)
                 {
                     layout.LayoutChanged += MinimumHeight.OnLayoutChanged;
-                    layout.PropertyChanged += MinimumHeight.OnPropertyChanged;
                 }
             }
 
             if (bindable is VisualElement visualElement)
             {
-                if (oldMinimumHeight != null)
+                if (oldValue != null)
                 {
                     visualElement.SizeChanged -= MinimumHeight.OnLayoutChanged;
-                    visualElement.PropertyChanged -= MinimumHeight.OnPropertyChanged;
                 }
 
                 if (newValue != null)
                 {
                     visualElement.SizeChanged += MinimumHeight.OnLayoutChanged;
-                    visualElement.PropertyChanged += MinimumHeight.OnPropertyChanged;
+                }
+            }
+
+            if (bindable is INotifyPropertyChanged notifyPropertyChanged)
+            {
+                if (oldValue != null)
+                {
+                    notifyPropertyChanged.PropertyChanged -= MinimumHeight.OnPropertyChanged;
+                }
+
+                if (newValue != null)
+                {
+                    notifyPropertyChanged.PropertyChanged += MinimumHeight.OnPropertyChanged;
                 }
             }
         }
 
         private static void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
         {
-            //if (e.PropertyName == "Height")
-            //{
-            //    RequestMinimumHeight(sender);
-            //}
+            if (e.PropertyName == "Text" && sender is VisualElement visualElement && visualElement.HeightRequest > 0)
+            {
+                // This code makes sure all VisualElements with a property update 'Text' and HeightRequest>0
+                // can grow their content (regardless of the MinimumHeight value).
+                Log(sender, $"OnPropertyChanged -> PropertyName={e.PropertyName}");
+                visualElement.HeightRequest = -1;
+                RequestMinimumHeight(sender);
+            }
         }
 
         private static void OnLayoutChanged(object sender, EventArgs e)
@@ -89,29 +102,33 @@ namespace CrossPlatformLibrary.Forms.Controls
             }
 
             var height = layout.Height;
+            if (height <= 0)
+            {
+                return;
+            }
+
             var minimumHeight = MinimumHeight.GetValue(layout);
-            if (minimumHeight <= 0 || height <= 0)
+            if (minimumHeight <= 0 || height == minimumHeight)
             {
                 return;
             }
 
             if (height < minimumHeight)
             {
-                Log($"RequestMinimumHeight: Requesting MinimumHeight={minimumHeight}");
+                Log(sender, $"Height={height} -> HeightRequest={minimumHeight}");
                 layout.HeightRequest = minimumHeight;
             }
-#if DEBUG
             else
             {
-                Log($"RequestMinimumHeight: HeightRequest not required (MinimumHeight={minimumHeight}, Height={height})");
+                Log(sender, $"Height={height} -> HeightRequest reset");
+                layout.HeightRequest = -1;
             }
-#endif
         }
 
         [Conditional("DEBUG")]
-        private static void Log(string message)
+        private static void Log(object element, string message)
         {
-            Debug.WriteLine($"MinimumHeight: RequestMinimumHeight: {message}");
+            Debug.WriteLine($"MinimumHeight for {type.GetFormattedName()}: {message}");
         }
     }
 }
