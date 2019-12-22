37732be6959ac1c0b5b634e4dcc63922860cb675
Thomas Galliker <thomas@bluewin.ch>
2019-12-22 15:50:31 +0100

    Add relative corner radius

---
+++ CrossPlatformLibrary.Forms/Converters/AddConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace CrossPlatformLibrary.Forms.Converters
+{
+    internal class AddConverter : BindableObject, IValueConverter
+    {
+        public static readonly BindableProperty ValueProperty =
+            BindableProperty.Create(
+                nameof(Value),
+                typeof(float),
+                typeof(AddConverter),
+                defaultValue: 0f);
+
+        public float Value
+        {
+            get => (float)this.GetValue(ValueProperty);
+            set => this.SetValue(ValueProperty, value);
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is float f)
+            {
+                return f + this.Value;
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
