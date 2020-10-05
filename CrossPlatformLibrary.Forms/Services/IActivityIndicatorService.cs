72be403cf384221526f6a569ede1eb60a4a0425c
Thomas Galliker <thomas@bluewin.ch>
2020-10-05 19:16:02 +0200

    Add ActivityIndicatorService for XF

---
+++ CrossPlatformLibrary.Forms/Services/IActivityIndicatorService.cs
@@ -0,0 +1,14 @@
+﻿using CrossPlatformLibrary.Forms.Controls;
+using Xamarin.Forms;
+
+namespace CrossPlatformLibrary.Forms.Services
+{
+    public interface IActivityIndicatorService
+    {
+        void Init<T>(T activityIndicatorPage) where T : ContentPage, IActivityIndicatorPage;
+
+        void ShowLoadingPage(string text);
+
+        void HideLoadingPage();
+    }
+}
