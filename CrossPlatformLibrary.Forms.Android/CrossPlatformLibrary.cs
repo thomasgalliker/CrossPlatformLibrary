53959933df235336c509a789cc2194b25977028b
Thomas Galliker <thomas@bluewin.ch>
2020-10-29 21:35:06 +0100

    Add SetCurrentActivityResolver for CrossPlatformLibrary on Android

--- CrossPlatformLibrary.Forms.Android/CrossPlatformLibrary.cs
+++ CrossPlatformLibrary.Forms.Android/CrossPlatformLibrary.cs
@@ -1,4 +1,7 @@
-﻿namespace CrossPlatformLibrary.Forms.Android
+﻿using System;
+using Android.App;
+
+namespace CrossPlatformLibrary.Forms.Android
 {
     public static class CrossPlatformLibrary
     {
@@ -8,6 +11,33 @@
         public static void Init()
         {
             //global::CrossPlatformLibrary.Forms.CrossPlatformLibrary.SetFontConverter(new FontConverter());
+        }
+
+        private static Func<Activity> activityResolver;
+
+        public static Activity CurrentActivity => GetCurrentActivity();
+
+        public static void SetCurrentActivityResolver(Func<Activity> activityResolver)
+        {
+            CrossPlatformLibrary.activityResolver = activityResolver;
+        }
+
+        private static Activity GetCurrentActivity()
+        {
+            if (activityResolver is null)
+            {
+                throw new InvalidOperationException("Resolver for the current activity is not set. Call CrossPlatformLibrary.SetCurrentActivityResolver somewhere in your startup code.");
+            }
+
+            var activity = activityResolver();
+            if (activity is null)
+            {
+                throw new InvalidOperationException("The configured CurrentActivityResolver returned null. " +
+                                                    "You need to setup the Android implementation via CrossPlatformLibrary.SetCurrentActivityResolver(). " +
+                                                    "If you are using CrossCurrentActivity don't forget to initialize it, too!");
+            }
+
+            return activity;
         }
     }
 }
