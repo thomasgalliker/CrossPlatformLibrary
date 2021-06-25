a66c77136d635f7cc9f57225892f5b0f0bf97586
Thomas Galliker <thomas@bluewin.ch>
2021-06-25 08:43:36 +0200

    Fix XmlnsDefinitions

--- CrossPlatformLibrary.Forms/Properties/AssemblyConfig.cs
+++ CrossPlatformLibrary.Forms/Properties/AssemblyConfig.cs
@@ -3,14 +3,15 @@ using Xamarin.Forms;
 
 [assembly: InternalsVisibleTo("CrossPlatformLibrary.Forms.Tests")]
 
-[assembly: XmlnsDefinition("http://superdev.ch/schemas/2020/cpl", "CrossPlatformLibrary.Behaviors")]
-[assembly: XmlnsDefinition("http://superdev.ch/schemas/2020/cpl", "CrossPlatformLibrary.Controls")]
-[assembly: XmlnsDefinition("http://superdev.ch/schemas/2020/cpl", "CrossPlatformLibrary.Converters")]
-[assembly: XmlnsDefinition("http://superdev.ch/schemas/2020/cpl", "CrossPlatformLibrary.Effects")]
-[assembly: XmlnsDefinition("http://superdev.ch/schemas/2020/cpl", "CrossPlatformLibrary.Extensions")]
 [assembly: XmlnsDefinition("http://superdev.ch/schemas/2020/cpl", "CrossPlatformLibrary.Localization")]
 [assembly: XmlnsDefinition("http://superdev.ch/schemas/2020/cpl", "CrossPlatformLibrary.Mvvm")]
 [assembly: XmlnsDefinition("http://superdev.ch/schemas/2020/cpl", "CrossPlatformLibrary.Resources")]
-[assembly: XmlnsDefinition("http://superdev.ch/schemas/2020/cpl", "CrossPlatformLibrary.Themes")]
-[assembly: XmlnsDefinition("http://superdev.ch/schemas/2020/cpl", "CrossPlatformLibrary.Tools")]
-[assembly: XmlnsDefinition("http://superdev.ch/schemas/2020/cpl", "CrossPlatformLibrary.Validation")]
+
+[assembly: XmlnsDefinition("http://superdev.ch/schemas/2020/cpl", "CrossPlatformLibrary.Forms.Behaviors")]
+[assembly: XmlnsDefinition("http://superdev.ch/schemas/2020/cpl", "CrossPlatformLibrary.Forms.Controls")]
+[assembly: XmlnsDefinition("http://superdev.ch/schemas/2020/cpl", "CrossPlatformLibrary.Forms.Converters")]
+[assembly: XmlnsDefinition("http://superdev.ch/schemas/2020/cpl", "CrossPlatformLibrary.Forms.Effects")]
+[assembly: XmlnsDefinition("http://superdev.ch/schemas/2020/cpl", "CrossPlatformLibrary.Forms.Extensions")]
+[assembly: XmlnsDefinition("http://superdev.ch/schemas/2020/cpl", "CrossPlatformLibrary.Forms.Themes")]
+[assembly: XmlnsDefinition("http://superdev.ch/schemas/2020/cpl", "CrossPlatformLibrary.Forms.Tools")]
+[assembly: XmlnsDefinition("http://superdev.ch/schemas/2020/cpl", "CrossPlatformLibrary.Forms.Validation")]
