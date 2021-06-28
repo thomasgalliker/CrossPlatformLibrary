3db9614551b1316f322f62d782436bfd11fd187b
Thomas Galliker <thomas@bluewin.ch>
2021-06-28 11:54:11 +0200

    Simplify code

--- CrossPlatformLibrary.Forms.Android/Effects/EntryLineColorEffect.cs
+++ CrossPlatformLibrary.Forms.Android/Effects/EntryLineColorEffect.cs
@@ -1,10 +1,11 @@
 using System;
 using System.ComponentModel;
-using System.Diagnostics;
+using Android.Graphics;
 using Android.Widget;
 using CrossPlatformLibrary.Forms.Effects;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
+using Debug = System.Diagnostics.Debug;
 using EntryLineColorEffect = CrossPlatformLibrary.Forms.Android.Effects.EntryLineColorEffect;
 
 [assembly: ExportEffect(typeof(EntryLineColorEffect), nameof(EntryLineColorEffect))]
@@ -12,25 +13,16 @@ namespace CrossPlatformLibrary.Forms.Android.Effects
 {
     public class EntryLineColorEffect : PlatformEffect
     {
-        EditText control;
-
         protected override void OnAttached()
         {
             try
             {
-                this.control = this.Control as EditText;
-
                 this.UpdateLineColor();
             }
             catch (Exception ex)
             {
                 Debug.WriteLine(ex.Message);
             }
-        }
-
-        protected override void OnDetached()
-        {
-            this.control = null;
         }
 
         protected override void OnElementPropertyChanged(PropertyChangedEventArgs args)
@@ -45,10 +37,10 @@ namespace CrossPlatformLibrary.Forms.Android.Effects
         {
             try
             {
-                if (this.control != null)
+                if (this.Control is EditText editText)
                 {
                     var lineColor = LineColorEffect.GetLineColor(this.Element);
-                    this.control.Background.SetColorFilter(lineColor.ToAndroid(), global::Android.Graphics.PorterDuff.Mode.SrcAtop);
+                    editText.Background.SetColorFilter(lineColor.ToAndroid(), PorterDuff.Mode.SrcAtop);
                 }
             }
             catch (Exception ex)
